cc3b3e447f8ad7b695340f756c6882a9f577b473
Salim Ingram <salim.ingram@gmail.com>
2023-06-09 21:56:04 +0100

    Added Global Secondary Index, list by status

--- services/tasks_api/store.py
+++ services/tasks_api/store.py
@@ -1,7 +1,8 @@
+import datetime
 from uuid import UUID
 
 import boto3
-
+from boto3.dynamodb.conditions import Key
 from models import Task, TaskStatus
 
 
@@ -16,6 +17,8 @@ class TaskStore:
             Item={
                 "PK": f"#{task.owner}",
                 "SK": f"#{task.id}",
+                "GS1PK": f"#{task.owner}#{task.status.value}",
+                "GS1SK": f"#{datetime.datetime.utcnow().isoformat()}",
                 "id": str(task.id),
                 "title": task.title,
                 "status": task.status.value,
@@ -38,3 +41,39 @@ class TaskStore:
             owner=record["Item"]["owner"],
             status=TaskStatus[record["Item"]["status"]],
         )
+
+    def list_open(self, owner):
+        return self._list_by_status(owner, TaskStatus.OPEN)
+
+    def list_closed(self, owner):
+        return self._list_by_status(owner, TaskStatus.CLOSED)
+
+    def _list_by_status(self, owner, status):
+        dynamodb = boto3.resource("dynamodb")
+        table = dynamodb.Table(self.table_name)
+        last_key = None
+        query_kwargs = {
+            "IndexName": "GS1",
+            "KeyConditionExpression": Key("GS1PK").eq(f"#{owner}#{status.value}"),
+        }
+        tasks = []
+        while True:
+            if last_key is not None:
+                query_kwargs["ExclusiveStartKey"] = last_key
+            response = table.query(**query_kwargs)
+            tasks.extend(
+                [
+                    Task(
+                        id=UUID(record["id"]),
+                        title=record["title"],
+                        owner=record["owner"],
+                        status=TaskStatus[record["status"]],
+                    )
+                    for record in response["Items"]
+                ]
+            )
+            last_key = response.get("lastEvaluatedKey")
+            if last_key is None:
+                break
+
+        return tasks
--- services/tasks_api/tests.py
+++ services/tasks_api/tests.py
@@ -3,11 +3,10 @@ import uuid
 import boto3
 import pytest
 from fastapi import status
+from main import app
+from models import Task, TaskStatus
 from moto import mock_dynamodb
 from starlette.testclient import TestClient
-
-from main import app
-from models import Task
 from store import TaskStore
 
 
@@ -31,11 +30,13 @@ def test_health_check(client):
 def dynamodb_table():
     with mock_dynamodb():
         client = boto3.client("dynamodb")
-        table_name = "test_table"
+        table_name = "test-table"
         client.create_table(
             AttributeDefinitions=[
                 {"AttributeName": "PK", "AttributeType": "S"},
                 {"AttributeName": "SK", "AttributeType": "S"},
+                {"AttributeName": "GS1PK", "AttributeType": "S"},
+                {"AttributeName": "GS1SK", "AttributeType": "S"},
             ],
             TableName=table_name,
             KeySchema=[
@@ -43,14 +44,48 @@ def dynamodb_table():
                 {"AttributeName": "SK", "KeyType": "RANGE"},
             ],
             BillingMode="PAY_PER_REQUEST",
+            GlobalSecondaryIndexes=[
+                {
+                    "IndexName": "GS1",
+                    "KeySchema": [
+                        {"AttributeName": "GS1PK", "KeyType": "HASH"},
+                        {"AttributeName": "GS1SK", "KeyType": "RANGE"},
+                    ],
+                    "Projection": {
+                        "ProjectionType": "ALL",
+                    },
+                },
+            ],
         )
         yield table_name
 
 
 def test_added_task_retrieved_by_id(dynamodb_table):
     repository = TaskStore(table_name=dynamodb_table)
-    task = Task.create(uuid.uuid4(), "Clean your office", "john@doe.com")
+    task = Task.create(uuid.uuid4(), "Sharpen your tools", "user@dev.com")
 
     repository.add(task)
 
     assert repository.get_by_id(task_id=task.id, owner=task.owner) == task
+
+
+def test_open_tasks_listed(dynamodb_table):
+    repository = TaskStore(table_name=dynamodb_table)
+    open_task = Task.create(uuid.uuid4(), "Sharpen your tools", "user@dev.com")
+    closed_task = Task(uuid.uuid4(), "Sharpen your tools", TaskStatus.CLOSED, "user@dev.com")
+
+    repository.add(open_task)
+    repository.add(closed_task)
+
+    assert repository.list_open(owner=open_task.owner) == [open_task]
+
+
+def test_closed_tasks_listed(dynamodb_table):
+    repository = TaskStore(table_name=dynamodb_table)
+    open_task = Task.create(uuid.uuid4(), "Sharpen your tools", "user@dev.com")
+    closed_task = Task(uuid.uuid4(), "Sharpen your tools", TaskStatus.CLOSED, "user@dev.com")
+
+    repository.add(open_task)
+    repository.add(closed_task)
+
+    assert repository.list_closed(owner=open_task.owner) == [closed_task]
