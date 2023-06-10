22abfd9a059edd49c883a558a506ceeb0808e0fe
Salim Ingram <salim.ingram@gmail.com>
2023-06-10 11:37:36 +0100

    Added DynamoDB URL to store.py for local DynamoDB support

--- services/tasks_api/store.py
+++ services/tasks_api/store.py
@@ -3,16 +3,16 @@ from uuid import UUID
 
 import boto3
 from boto3.dynamodb.conditions import Key
-
 from models import Task, TaskStatus
 
 
 class TaskStore:
-    def __init__(self, table_name):
+    def __init__(self, table_name, dynamodb_url=None):
         self.table_name = table_name
+        self.dynamodb_url = dynamodb_url
 
     def add(self, task):
-        dynamodb = boto3.resource("dynamodb")
+        dynamodb = boto3.resource("dynamodb", endpoint_url=self.dynamodb_url)
         table = dynamodb.Table(self.table_name)
         table.put_item(
             Item={
@@ -28,7 +28,7 @@ class TaskStore:
         )
 
     def get_by_id(self, task_id, owner):
-        dynamodb = boto3.resource("dynamodb")
+        dynamodb = boto3.resource("dynamodb", endpoint_url=self.dynamodb_url)
         table = dynamodb.Table(self.table_name)
         record = table.get_item(
             Key={
@@ -50,7 +50,7 @@ class TaskStore:
         return self._list_by_status(owner, TaskStatus.CLOSED)
 
     def _list_by_status(self, owner, status):
-        dynamodb = boto3.resource("dynamodb")
+        dynamodb = boto3.resource("dynamodb", endpoint_url=self.dynamodb_url)
         table = dynamodb.Table(self.table_name)
         last_key = None
         query_kwargs = {
